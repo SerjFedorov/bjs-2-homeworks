fcd56c64afc64d629ce88ce4bf20c972ae889bb4
Sergey Fedorov <fedyaser@inbox.ru>
2023-01-24 17:21:57 +0530

    first done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,15 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = (b**2) - (4*a*c);
+  if (discriminant === 0) {
+    let rootSingle = -b/(2*a);
+    arr.push(rootSingle);
+  } else if (discriminant > 0 ) {
+    let rootOne = (-b + Math.sqrt(discriminant) )/(2*a);
+    let rootSecond = (-b - Math.sqrt(discriminant) )/(2*a);
+    arr.push(rootOne, rootSecond);
+  }
   return arr;
 }
 
