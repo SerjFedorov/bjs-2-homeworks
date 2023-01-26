c29669219ce26a78cbd84551af4d12adca649bd8
Sergey Fedorov <fedyaser@inbox.ru>
2023-01-27 00:14:16 +0530

    All task compl with upd

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -4,12 +4,13 @@ function compareArrays(arr1, arr2) {
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-    let resultRange = users.filter(users => users.gender === gender).reduce((acc, ageAvg, index, arrayLength) => acc + ageAvg.age /arrayLength.length, 0);
+    let resultRange = users.filter(users => users.gender === gender).reduce((acc, ageAvg, index, arr) => acc + ageAvg.age /arr.length, 0);
     return resultRange;
 }
 
 //users => users.gender === gender в данном случае users новая переменная не равная в функции users
 //users => users.gender === gender равно el=>el.gender===gender фильтрация по полу
-//без index не работает
-//acc + ageAvg.age /arrayLength.length вычисление среднего возраста путем reduce 
-// acc - начальное значение ,ageAvg.age -значение возраста в массиве ,arrayLength.length колво человек
+//без index не работает так как нужно выбрать массив,а это третья позиция в .reduce((accumulator)(item)(index)(arrr))
+//acc + ageAvg.age /arr.length вычисление среднего возраста путем reduce 
+// acc - начальное значение ,ageAvg.age -значение возраста в массиве ,arr.length колво человек
+//0 в конце начальное значение acc
