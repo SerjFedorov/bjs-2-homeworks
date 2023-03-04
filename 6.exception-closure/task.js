bd4decf3a4b6aa3ac801a7f61320ca295e827216
Sergey Fedorov <fedyaser@inbox.ru>
2023-03-04 16:09:25 +0530

    task 1 compl

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,23 @@
-﻿
+﻿function parseCount (parseNumber){
+    let result = Number.parseFloat(parseNumber);
+        if (Number.isNaN(result)) throw new Error('Невалидное значение');
+        return result;  
+}
+
+function validateCount (b) {
+    try {
+        return parseCount(b)
+    }
+    catch(err) {
+        return err
+    }  
+};
+
+class Triangle {
+    constructor (a,b,c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    if 
+}
