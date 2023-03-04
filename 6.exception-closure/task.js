6669a1a2ed8a6965437d4742e84889182398990b
Sergey Fedorov <fedyaser@inbox.ru>
2023-03-04 17:52:54 +0530

    task 2 complete

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -18,6 +18,32 @@ class Triangle {
         this.a = a;
         this.b = b;
         this.c = c;
+    
+    if (((a + b) < c) || ((a + c) < b) || ((b + c) < a)){
+        throw new Error ("Треугольник с такими сторонами не существует");
+        }    
     }
-    if 
+    get perimeter () {
+        return this.a + this.b + this.c;
+    }
+
+    get area () {
+        let p = this.perimeter/2;
+        return Number (Math.sqrt(p*(p - this.a)*(p - this.b)*(p - this.c)).toFixed(3));
+    }
+}
+
+function getTriangle (a,b,c) {
+    try {
+    return new Triangle(a,b,c)
+    } catch (error) {
+        return {
+        get area () {
+        return 'Ошибка! Треугольник не существует'
+        },
+        get perimeter () {
+        return 'Ошибка! Треугольник не существует'
+        }
+    }
+    }
 }
